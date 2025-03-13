abe3e54d050139baf89b21511b5592c341284c38
zestones <idrissbenguezzou@gmail.com>
2025-03-13 17:47:26 +0100

    fix: include config.js to add network address

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -4,14 +4,14 @@ const config = { wsUrl: 'ws://localhost:3000', clientUrl: 'http://localhost:5175
 
 // Dynamically import the IP configuration
 import('./ipConfig.js').then(ipConfig => {
-    if (ipConfig.IP_ADDRESS) {
+    if (ipConfig) {
         // Use environment variable with a fallback to 'dev'
         const mode = process.env.NODE_ENV || 'dev';
 
-        const localNetwork = ipConfig.IP_ADDRESS;
-        const localhost = 'ws://localhost:3000';
+        const localNetwork = ipConfig.LOCAL_ADDRESSE;
+        const prodNetwork = ipConfig.PROD_ADDRESS
 
-        config.wsUrl = mode === 'dev' ? localhost : localNetwork;
+        config.wsUrl = mode === 'dev' ? localNetwork : prodNetwork;
         console.info("Webserver url : " + config.wsUrl)
     }
 }).catch(error => {
