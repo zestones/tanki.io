59120f0de59f8e40e5a69dbf5563e0d2166a441e
zestones <idrissbenguezzou@gmail.com>
2025-03-13 17:42:32 +0100

    add: deploy pipeline

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -5,12 +5,14 @@ const config = { wsUrl: 'ws://localhost:3000', clientUrl: 'http://localhost:5175
 // Dynamically import the IP configuration
 import('./ipConfig.js').then(ipConfig => {
     if (ipConfig.IP_ADDRESS) {
-        let mode = 'prod';
+        // Use environment variable with a fallback to 'dev'
+        const mode = process.env.NODE_ENV || 'dev';
 
-        const localNetwork = `ws://${ipConfig.IP_ADDRESS}:3000`;
+        const localNetwork = ipConfig.IP_ADDRESS;
         const localhost = 'ws://localhost:3000';
 
         config.wsUrl = mode === 'dev' ? localhost : localNetwork;
+        console.info("Webserver url : " + config.wsUrl)
     }
 }).catch(error => {
     console.error('Failed to load IP configuration:', error);
